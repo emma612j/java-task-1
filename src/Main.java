187fd549adf5e89cd9a140bccf2bd5fd7c23cb23
Emmanuel <emmanuelfidelis.ken@gmail.com>
2025-02-25 15:42:42 +0100

    added looping 1 to 5

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.print("Hello Vistula");
-
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
     }
 }
